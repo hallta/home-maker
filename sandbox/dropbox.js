dd1af59495aa05c112000b210c7ed3ac9a077752
Trevor Hall <trhall@tableau.com>
2017-02-08 23:10:33 -0800

    deleted app and removed token

--- sandbox/dropbox.js
+++ sandbox/dropbox.js
@@ -1,9 +1,10 @@
 var Dropbox = require('dropbox');
-var dbx = new Dropbox({ accessToken: 'SpYgOK-oZXQAAAAAAAB4MxSY-ezl6oV0lV4u-vSfG0CznMII9-hpb5_A8SKz4_v4' });
+var dbx = new Dropbox({ accessToken: '' });
 
 dbx.filesListFolder({path: ''})
   .then(function(response) {
           console.log(response);
+        // make a new app and hide the token!! 
         //iterate over files, filesDownload(arg) -- http://dropbox.github.io/dropbox-sdk-js/global.html#FilesDownloadArg
             })
   .catch(function(error) {
